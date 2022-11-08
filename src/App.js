93892a4a4d1539a977e74cedef7a0634974edf39
ruoxinhe <ruoxin.he@mail.utoronto.ca>
2022-11-07 21:14:24 -0500

    made changes using routers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import videodetail from "./data/video-details.json";
 import videodata from "./data/videos.json";
@@ -7,6 +8,7 @@ import MainVideo from "./components/MainVideo/MainVideo";
 import Videoview from "./components/Videoview/Videoview";
 import Comments from "./components/Comments/Comments";
 import Videos from "./components/Videos/Videos";
+import Video from "./components/Video/Video";
 import "./assets/Font/AvenirNextLTPro-Bold.otf";
 import "./assets/Font/AvenirNextLTPro-Regular.otf";
 
@@ -22,13 +24,20 @@ function App() {
 
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Video />} />
+          <Route path="/video/:id" element={<Video />} />
+        </Routes>
+      </BrowserRouter>
       <Videoview currentvideo={selectedVideo} />
       <div className="videoinfo">
         <div className="individeo">
           <MainVideo currentvideo={selectedVideo} />
           <Comments currentvideo={selectedVideo} />
         </div>
+
         <Videos
           videoList={videodata}
           currentVideo={selectedVideo}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import brainflixLogo from "../../assets/Logo/BrainFlix-logo.svg";
 import avatar from "../../assets/Images/Mohan-muruge.jpg";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <div className="header">
-        <img className="header__logo" alt="logo" src={brainflixLogo} />
+        <Link to="/">
+          <img className="header__logo" alt="logo" src={brainflixLogo} />
+        </Link>
         <div className="header__search">
           <input type="text" className="header__input" placeholder="Search" />
           <img src={avatar} alt="search" className="header__avatar" />
