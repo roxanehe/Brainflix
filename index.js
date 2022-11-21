b9f6d7fc6b7d8f4c6f52710ad2ac406d8479a55b
ruoxinhe <ruoxin.he@mail.utoronto.ca>
2022-11-20 20:03:40 -0500

    add port in env file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
